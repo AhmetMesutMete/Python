31f24dfd8c1f5d13198797c8539e75a6f8378f86
Ahmet Mesut METE <69396993+AhmetMesutMete@users.noreply.github.com>
2020-09-22 21:08:12 +0200

    Update hr_day13.py

--- hacker_rank_30days/hr_day13.py
+++ hacker_rank_30days/hr_day13.py
@@ -46,9 +46,7 @@ class Book(object, metaclass=ABCMeta):
 #Write MyBook class
 class MyBook(Book):
     def __init__(self, title, author, price):
-        Book.__init__(self, title, author)
-        self.title = title
-        self.author = author
+        super().__init__(title, author)
         self.price = price
     def display(self):
         print(f"Title: {self.title}\nAuthor: {self.author}\nPrice: {self.price}")
