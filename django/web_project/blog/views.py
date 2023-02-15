2c626eff543aed9c5da877007714502dad09088f
Ahmet Mesut METE <69396993+AhmetMesutMete@users.noreply.github.com>
2023-02-15 21:18:57 +0000

    upload file (image)

--- django/web_project/blog/views.py
+++ django/web_project/blog/views.py
@@ -2,43 +2,6 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from blog.models import Blog
 
-data = {
-    'blogs': [
-        {
-            'id': 1,
-            'title': 'Programming From the Scratch',
-            'image': '1.jpg',
-            'is_active': True,
-            'is_home': False,
-            'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eos cum ipsa quisquam quasi'
-        },
-        {
-            'id': 2,
-            'title': 'Python Web Development Tutorial',
-            'image': '4.jpg',
-            'is_active': True,
-            'is_home': True,
-            'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eos cum ipsa'
-        },
-        {
-            'id': 3,
-            'title': 'Django Tutorial',
-            'image': '2.png',
-            'is_active': False,
-            'is_home': True,
-            'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eos'
-        },
-        {
-            'id': 4,
-            'title': 'React for Beginners',
-            'image': '3.png',
-            'is_active': True,
-            'is_home': True,
-            'description': 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, eos cum ipsa quisquam quasi aut eius numquam'
-        }
-    ]
-}
-
 
 def index(request):
     context = {
