ae2b5b87e098c16cd09c1e68a9391f64868363fa
Ahmet Mesut METE <69396993+AhmetMesutMete@users.noreply.github.com>
2023-05-10 17:44:14 +0000

    add dynamic category menu

--- django/web_project/blog/views.py
+++ django/web_project/blog/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from blog.models import Blog
+from blog.models import Blog, Category
 
 
 def index(request):
     context = {
-        'blogs': Blog.objects.filter(is_active=True, is_home=True)
+        'blogs': Blog.objects.filter(is_active=True, is_home=True),
+        'categories': Category.objects.all()
     }
     return render(request, "blog/index.html", context)
 
 
 def blogs(request):
     context = {
-        'blogs': Blog.objects.filter(is_active=True)
+        'blogs': Blog.objects.filter(is_active=True),
+        'categories': Category.objects.all()
     }
     return render(request, "blog/blogs.html", context)
 
@@ -20,3 +22,8 @@ def blogs(request):
 def blog_details(request, slug):
     blog =  Blog.objects.get(slug=slug)
     return render(request, "blog/blog-details.html", {'blog':blog})
+
+
+def blogs_by_category(request, slug):
+    pass
+
